0633a29686a3424dac6a516b565b9aadb3122e1b
OswarCruz <moises.cruzn499@gmail.com>
2023-08-26 00:19:03 -0600

    editar dattos del usuario

--- src/app/components/profile-components/user-profile-edit/user-profile-edit.component.ts
+++ src/app/components/profile-components/user-profile-edit/user-profile-edit.component.ts
@@ -19,19 +19,41 @@ export class UserProfileEditComponent implements OnInit {
     name:"",
     lastname:"",
     pass:"",
-    img:""
-  }
+    pass_ver:"",
+    img:"",
+    type: ""
+  };
+
+  pass_mod: boolean = false;
+  pass_alert: boolean = false;
+  pass_len: boolean = true;
 
   ch: UseChange = new UseChange(this.states);
 
   constructor (private router: Router){}
+
   ngOnInit(): void {
     this.ch.updateValues({
       ...this.states,
       name: this.user?.name,
       lastname: this.user?.lastname,
-      img: this.user?.img
-    })
+      img: this.user?.img,
+      type: this.user?.type,
+    });
+    this.selected = this.user?.type!;
+  }
+
+  fnComparePass(event: any){
+    this.pass_mod = true;
+    this.ch.handleChange(event);
+    const values = this.ch.getValues();
+
+    const text: string = values.pass_ver;
+    const pass: string = values.pass;
+
+    this.pass_alert = (text !== pass);
+    this.pass_len= (event.target.name==="pass" && event.target.value.length < 8);
+    
   }
 
   fnCancel(){
@@ -67,6 +89,31 @@ export class UserProfileEditComponent implements OnInit {
       };
       this.loading = false;
     })
+  };
+
+  selected: string = "GRATUITO";
+  
+  types: string[] = ["GRATUITO", "ESTANDAR", "PROFESIONAL"];
+
+  setSelected(type: 0|1|2){
+    this.selected = this.types[type];
+    this.ch.handleChange({target:{
+      name:"type",
+      value:this.types[type]
+    }})
+  };
+
+  getIsSelected(type: 0|1|2){
+    return this.selected === this.types[type];
+  };
+
+  getStyle(type: 0|1|2){
+    const s = "border-style: solid; border-color: white;"
+    if(this.getIsSelected(type)){
+      return s;
+    }else{
+      return "";
+    }
   }
 
 }
